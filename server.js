aacc94d607c7891b66eeff63d1190e09ccd373ea
arjun bala <arjun.bala@darshan.ac.in>
2025-11-06 12:14:55 +0530

    changed testing

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get("/", (req, res) => res.send("Hello Testing"));
 app.use("/api/auth", authRoutes);
 app.use("/api/contact", contactRoutes);
 
