1e563685528872f1903620f2190776c11056a4c5
arjun bala <arjun.bala@darshan.ac.in>
2025-11-06 12:51:12 +0530

    login debug

--- controllers/authController.js
+++ controllers/authController.js
@@ -69,6 +69,7 @@ export const login = async (req, res) => {
 
     res.json({ message: "Login successful", token });
   } catch (err) {
+    console.log(err);
     res.status(500).json({ message: "Server error", error: err.message });
   }
 };
